752e37e0e1d998e3d4a53c5db8ddcaff7fe0d7d1
Konrad Matusiak <matusiak7konrad@gmail.com>
2024-08-04 22:21:27 +0200

    added add desk command class

---
+++ EasyBooking/Commands/AddDeskCommand.cs
@@ -0,0 +1,14 @@
+﻿using EasyBooking.Dto;
+using MediatR;
+
+namespace EasyBooking.Commands;
+
+public class AddDeskCommand : IRequest<OfficeLocationDto>
+{
+    public int OfficeLocationId { get; }
+
+    public AddDeskCommand(int officeLocationId)
+    {
+        OfficeLocationId = officeLocationId;
+    }
+}

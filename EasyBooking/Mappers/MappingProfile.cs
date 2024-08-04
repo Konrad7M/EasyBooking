a970d31f3a439e7ddb39011bbd5183562e641151
Konrad Matusiak <matusiak7konrad@gmail.com>
2024-08-04 22:13:05 +0200

    Added Mappings for office locations

---
+++ EasyBooking/Mappers/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EasyBooking.Api.Dto;
+using EasyBooking.Domain.Model;
+using EasyBooking.Dto;
+
+namespace EasyBooking.Api.Mappers;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Desk, DeskDto>();
+        CreateMap<OfficeLocation, OfficeLocationDto>();
+    }
+}

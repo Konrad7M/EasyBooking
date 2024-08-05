005dfc3e2de3815076e083bf712bd04f4deeaee2
Konrad Matusiak <matusiak7konrad@gmail.com>
2024-08-05 22:15:34 +0200

    Add mapping for reservations

--- EasyBooking/Mappers/MappingProfile.cs
+++ EasyBooking/Mappers/MappingProfile.cs
@@ -14,5 +14,7 @@ public class MappingProfile : Profile
         CreateMap<OfficeLocation, OfficeLocationDto>();
         CreateMap<Employee, EmployeeDto>();
         CreateMap<GetDesksCommand, GetDesksQueryDto>();
+        CreateMap<Reservation, ReservationDto>();
+        
     }
 }
